ad4097cdf0c7e9c2085a5532de52771703ee020b
nick <n.carandini@outlook.com>
2020-05-17 14:16:10 +0200

    Add Xamarin Forms App

---
+++ TPCWare.AuthDemo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TPCWare.AuthDemo.Models;
+
+namespace TPCWare.AuthDemo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
